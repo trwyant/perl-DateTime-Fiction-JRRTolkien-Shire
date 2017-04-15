88f0db14b27c47bb1096ef1fea52b3f8e7322963
Tom Wyant <wyant@cpan.org>
2017-04-15 14:28:27 -0400

    Version 0.901_01
    Set version to 0.901_01. Update Changes file for changes since v00.901.

--- lib/DateTime/Fiction/JRRTolkien/Shire/Duration.pm
+++ lib/DateTime/Fiction/JRRTolkien/Shire/Duration.pm
@@ -22,7 +22,7 @@ use overload
     'cmp'	=> '_compare_overload',
     ;
 
-our $VERSION = '0.901';
+our $VERSION = '0.901_01';
 
 {
 
--- lib/DateTime/Fiction/JRRTolkien/Shire/Types.pm
+++ lib/DateTime/Fiction/JRRTolkien/Shire/Types.pm
@@ -18,7 +18,7 @@ use Specio::Library::Builtins;
 use Specio::Library::Numeric;
 use Specio::Library::String;
 
-our $VERSION = '0.901';
+our $VERSION = '0.901_01';
 
 declare(
     DayOfMonth	=>
