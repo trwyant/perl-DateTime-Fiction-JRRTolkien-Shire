4b0ddc09a60f10fc9421501674e9ea8fbad34e19
Tom Wyant <wyant@cpan.org>
2019-10-23 17:00:19 -0400

    Eliminate redirections in POD URL links

--- lib/DateTime/Fiction/JRRTolkien/Shire/Duration.pm
+++ lib/DateTime/Fiction/JRRTolkien/Shire/Duration.pm
@@ -471,7 +471,7 @@ L<DateTime::Duration|DateTime::Duration>
 =head1 SUPPORT
 
 Support is by the author. Please file bug reports at
-L<http://rt.cpan.org>, or in electronic mail to the author.
+L<https:rt.cpan.org>, or in electronic mail to the author.
 
 =head1 AUTHOR
 
