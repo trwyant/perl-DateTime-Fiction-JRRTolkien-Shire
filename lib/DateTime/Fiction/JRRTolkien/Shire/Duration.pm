64f82ccdf267ca9914ff3695659b42f7b5ae9210
Tom Wyant <wyant@cpan.org>
2021-03-07 20:25:24 -0500

    Add rt.cpan.org back to bug reporting methods

--- lib/DateTime/Fiction/JRRTolkien/Shire/Duration.pm
+++ lib/DateTime/Fiction/JRRTolkien/Shire/Duration.pm
@@ -471,8 +471,9 @@ L<DateTime::Duration|DateTime::Duration>
 =head1 SUPPORT
 
 Support is by the author. Please file bug reports at
-L<https://github.com/trwyant/perl-DateTime-Fiction-JRRTolkien-Shire/issues>,
-or in electronic mail to the author.
+L<https://rt.cpan.org/Public/Dist/Display.html?Name=DateTime-Fiction-JRRTolkien-Shire>,
+L<https://github.com/trwyant/perl-DateTime-Fiction-JRRTolkien-Shire/issues>, or in
+electronic mail to the author.
 
 =head1 AUTHOR
 
